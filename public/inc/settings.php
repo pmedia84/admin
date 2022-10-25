ee62ecb1a08f9465c7159c7e08ee5681eabd1c4f
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2022-10-25 08:48:58 +0100

    basic setup

---
+++ public/inc/settings.php
@@ -0,0 +1,20 @@
+<?php
+//email settings for contact forms
+//Settings for all form scripts
+/// Define who the emails get sent to from forms filled out
+$email_to = "luke@boltonswindowcleaning.co.uk";
+
+$host = "mail.boltonswindowcleaning.co.uk"; /// Hostname
+$username = "admin@boltonswindowcleaning.co.uk"; ///Username
+$pass = "rKrltTuvlQ"; /// Password
+$from = $username; ///Email
+
+$fromname = "Webmaster - Bolton's Window Cleaning"; /// Username and how you want your name to be displayed on emails
+$emailheaderlogo = "";//logo url for inserting into the top of email bodies
+
+//Contact details: Phone number, email address etc
+$contactnum ="";
+$emailaddress = "";
+$facebookurl = "";
+?>
+
