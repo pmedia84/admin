61a857acc13b22c6a1d7eb91ead80fb6d2bd2f7f
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2022-12-12 12:31:10 +0000

    c

--- resetpw.php
+++ resetpw.php
@@ -1,5 +1,4 @@
 <?php
-
 session_start();
 if (isset($_GET['user_email'])) {
     $user_email = $_GET['user_email'];
--- setup.php
+++ setup.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 $business_id = "1";
 $wedding_id = "1";
 if (empty($_GET)) {
