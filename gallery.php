682f6851f7ba4bfc1e39d97a0ac85547702c09aa
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2023-02-01 20:16:34 +0000

    uploading images

--- gallery.php
+++ gallery.php
@@ -72,7 +72,7 @@ if ($cms_type == "Wedding") {
 
 <!-- / -->
 <!-- Page Title -->
-<title>Mi-Admin | News</title>
+<title>Mi-Admin | Photo Gallery</title>
 <!-- /Page Title -->
 </head>
 
@@ -97,7 +97,7 @@ if ($cms_type == "Wedding") {
                 <h1>Image Gallery</h1>
 
                 <?php if ($user_type == "Admin" || $user_type=="Developer") : ?>
-                    <button class="btn-primary" id="upload_image"><i class="fa-solid fa-upload"></i>Upload Image </button>
+                    <a class="btn-primary" href="image?action=create"><i class="fa-solid fa-upload"></i>Upload Image </a>
                     <div id="img-gallery">
 
 
@@ -113,65 +113,7 @@ if ($cms_type == "Wedding") {
     <div class="loader">
         <img class="loader-spinner" src="./assets/img/icons/loading.svg" alt="">
     </div>
-    <div class="modal">
-        <div class="modal-body">
-            <div class="modal-close">
-                <button type="button" class="btn-close" id="modal-btn-close" aria-label="Close"></button>
-            </div>
-            <h2>Upload New Image</h2>
-            <form action="scripts/gallery.script.php" id="img-upload" method="post" enctype="multipart/form-data">
-                <div class="form-input-wrapper">
-                    <label for="image_title">Image Title</label>
-                    <!-- input -->
-                    <input class="text-input input" type="text" name="image_title" id="image_title" placeholder="Image Title" maxlength="45">
-                </div>
-                <div class="form-input-wrapper">
-                    <label for="image_description">Image Description</label>
-                    <!-- input -->
-                    <input class="text-input input" type="text" name="image_description" id="image_description" placeholder="Image Description" maxlength="45">
-                </div>
-                <div class="form-input-wrapper my-2">
-                    <label for="gallery_img">Upload Image</label>
-                    <p class="form-hint-small">This can be in a JPG, JPEG or PNG format. And no larger than 1MB.</p>
-                    <!-- input -->
-                    <input type="file" name="gallery_img" id="gallery_img" accept="image/*">
-                </div>
-
-                <h3>Image Placement</h3>
-                <div class="my-2">
-                    
-                        
-                        <label class="checkbox-form-control" for="home">
-                        <input type="checkbox" id="home" name="img_placement[]" value="Home"/>
-                        Home Screen
-                        </label>
-                   
-                        
-                        <label class="checkbox-form-control" for="gallery">
-                        <input type="checkbox" id="gallery" name="img_placement[]" value="Gallery"/>    
-                        Photo Gallery
-                        </label>
-                        
-                  
-
-                </div>
-
-                <div class="modal-button-section">
-                    <button class="btn-primary form-controls-btn loading-btn" type="submit"><span id="loading-btn-text" class="loading-btn-text"><i class="fa-solid fa-upload"></i>Upload Image</span> <img id="loading-icon" class="loading-icon d-none" src="./assets/img/icons/loading.svg" alt=""></button>
-                    <button class="btn-primary btn-secondary form-controls-btn" id="cancel" type="button"><i class="fa-solid fa-ban"></i>Cancel</button>
-                </div>
-            </form>
-
-            <div id="response" class="d-none">
-
-
-
-            </div>
-        </div>
-
-
-        </form>
-    </div>
+    
     </div>
     <!-- /Main Body Of Page -->
 
@@ -195,51 +137,7 @@ if ($cms_type == "Wedding") {
             });
         })
     </script>
-    <script>
-        //script for uploading a new image and posting to backend article
-        $("#img-upload").submit(function(event) {
-            event.preventDefault();
 
-
-            var formData = new FormData($("#img-upload").get(0));
-
-            formData.append("action", "newimg");
-
-            $.ajax({ //start ajax post
-                type: "POST",
-                url: "scripts/gallery.script.php",
-                data: formData,
-                contentType: false,
-                processData: false,
-                beforeSend: function() { //animate button
-                    $("#loading-icon").show(400);
-                },
-                complete: function() {
-                    $("#loading-icon").hide(400);
-                },
-                success: function(data, responseText) {
-
-                    $("#img-upload")[0].reset();
-                    url = "scripts/gallery.script.php?action=loadgallery";
-                    $.ajax({ //load image gallery
-                        type: "GET",
-                        url: url,
-                        encode: true,
-
-                        success: function(data, responseText) {
-                            $("#img-gallery").html(data);
-                        }
-                    });
-                    $("#response").html(data);
-                    $("#response").slideDown(400);
-                    if(data ==="success"){
-                        $(".modal").removeClass("modal-active");
-                    }
-                }
-            });
-
-        });
-    </script>
 </body>
 
 </html>
--- image.php
+++ image.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
-$location=$_SERVER['REQUEST_URI'];
-$location=urlencode($_SERVER['REQUEST_URI']);
+$location = $_SERVER['REQUEST_URI'];
+$location = urlencode($_SERVER['REQUEST_URI']);
 if (!$_SESSION['loggedin'] == TRUE) {
     // Redirect to the login page:
-    
-    header("Location: login.php?location=".$location);
+
+    header("Location: login.php?location=" . $location);
 }
 
 include("connect.php");
@@ -16,7 +16,7 @@ include("inc/settings.php");
 //Variable for name of CMS
 //wedding is the name of people
 //business name
-$cms_name ="";
+$cms_name = "";
 $user_id = $_SESSION['user_id'];
 if ($cms_type == "Business") {
     //look for the business set up and load information
@@ -31,10 +31,10 @@ if ($cms_type == "Business") {
     //set cms name
     $cms_name = $business_name;
     //find user details for this business
-    $business_users = $db->prepare('SELECT users.user_id, users.user_name, business_users.business_id, business_users.user_type FROM users NATURAL LEFT JOIN business_users WHERE users.user_id='.$user_id);
+    $business_users = $db->prepare('SELECT users.user_id, users.user_name, business_users.business_id, business_users.user_type FROM users NATURAL LEFT JOIN business_users WHERE users.user_id=' . $user_id);
 
     $business_users->execute();
-    $business_users->bind_result($user_id, $user_name,$business_id, $user_type);
+    $business_users->bind_result($user_id, $user_name, $business_id, $user_type);
     $business_users->fetch();
     $business_users->close();
 }
@@ -59,18 +59,21 @@ if ($cms_type == "Wedding") {
     $wedding_users->bind_result($user_id, $user_name, $wedding_id, $user_type);
     $wedding_users->fetch();
     $wedding_users->close();
-
 }
 
 //////////////////////////////////////////////////////////////////Everything above this applies to each page\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 //image variable
-$image_id = $_GET['image_id'];
-//find image details
+if (isset($_GET['image_id'])) {
+    $image_id = $_GET['image_id'];
+    //find image details
 
-$image = $db->prepare('SELECT * FROM images WHERE image_id =' . $image_id);
+    $image = $db->prepare('SELECT * FROM images WHERE image_id =' . $image_id);
 
-$image->execute();
-$image->store_result();
+    $image->execute();
+    $image->store_result();
+}
+
+
 
 
 ?>
@@ -90,7 +93,7 @@ $image->store_result();
     <!-- Main Body Of Page -->
     <main class="main col-2">
         <!-- Header Section -->
-        <?php include("inc/header.inc.php");?>
+        <?php include("inc/header.inc.php"); ?>
         <!-- Nav Bar -->
         <?php include("./inc/nav.inc.php"); ?>
         <!-- /nav bar -->
@@ -119,11 +122,8 @@ $image->store_result();
                     <h1>Delete Image</h1>
                 <?php endif; ?>
 
-                <?php if ($_GET['action'] == "edit") : ?>
-                    <p class="font-emphasis">This page is best viewed on a large screen</p>
-                <?php else : ?>
-                <?php endif; ?>
-                <?php if ($user_type == "Admin" ||$user_type=="Developer") : //detect if user is an admin or not 
+
+                <?php if ($user_type == "Admin" || $user_type == "Developer") : //detect if user is an admin or not 
                 ?>
                     <?php if ($_GET['action'] == "delete") : //if action is delete, detect if the confirm is yes or no
                     ?>
@@ -135,12 +135,12 @@ $image->store_result();
                                 // connect to db and delete the record
                                 $delete_image = "DELETE FROM images WHERE image_id=" . $image_id;
                                 //delete image on server
-                                $file = $_SERVER['DOCUMENT_ROOT']."/admin/assets/img/gallery/".$image_filename;
-                                $gallery = $_SERVER['DOCUMENT_ROOT']."/assets/img/gallery/".$image_filename;
-                                if(fopen($file,"w")){
+                                $file = $_SERVER['DOCUMENT_ROOT'] . "/admin/assets/img/gallery/" . $image_filename;
+                                $gallery = $_SERVER['DOCUMENT_ROOT'] . "/assets/img/gallery/" . $image_filename;
+                                if (fopen($file, "w")) {
                                     unlink($file);
                                 };
-                                if(fopen($gallery,"w")){
+                                if (fopen($gallery, "w")) {
                                     unlink($gallery);
                                 };
                                 if (mysqli_query($db, $delete_image)) {
@@ -149,20 +149,20 @@ $image->store_result();
                                     echo '<div class="form-response error"><p>Error deleting image, please try again.</p></div>';
                                 }
                             ?>
-                        <?php else : ?>
-                            <div class="std-card">
-                                <h2>Error</h2>
-                                <p>There has been an error, please return to the last page and try again.</p>
-                            </div>
+                            <?php else : ?>
+                                <div class="std-card">
+                                    <h2>Error</h2>
+                                    <p>There has been an error, please return to the last page and try again.</p>
+                                </div>
                             <?php endif; ?>
                         <?php else : //if not then display the message to confirm the user wants to delete the news article
                         ?>
                             <?php if (($image->num_rows) > 0) :
                                 $image->bind_result($image_id, $image_title, $image_description, $image_filename, $image_upload_date, $image_placement);
                                 $image->fetch();
-                                
-                                
-                                
+
+
+
                             ?>
                                 <div class="std-card">
                                     <h2 class="text-alert">Delete: <?= $image_title; ?></h2>
@@ -189,7 +189,7 @@ $image->store_result();
 
                         ?>
                             <div class="std-card">
-                                <form class="form-card" id="edit_image" action="scripts/gallery.script.php" method="POST" enctype="multipart/form-data">
+                                <form id="edit_image" action="scripts/gallery.script.php" method="POST" enctype="multipart/form-data">
                                     <div class="form-input-wrapper">
 
                                         <p><strong>File Name:</strong> <?= $image_filename; ?></p>
@@ -210,13 +210,13 @@ $image->store_result();
                                         <h2>Image Placement</h2>
                                         <p class="form-hint-small my-2">This determines where you image will be displayed on your website. You can use the same image in different locations if you wish.</p>
                                         <label class="checkbox-form-control" for="home">
-                                            <input type="checkbox" id="home" name="img_placement[]" value="Home" <?php if (strpos($image_placement, "Home") !== FALSE ):?>Checked <?php endif;?>/>
+                                            <input type="checkbox" id="home" name="img_placement[]" value="Home" <?php if (strpos($image_placement, "Home") !== FALSE) : ?>Checked <?php endif; ?> />
                                             Home Screen
                                         </label>
 
 
                                         <label class="checkbox-form-control" for="gallery">
-                                            <input type="checkbox" id="gallery" name="img_placement[]" value="Gallery" <?php if (strpos($image_placement, "Gallery") !== FALSE) :?>Checked <?php endif; ?> />
+                                            <input type="checkbox" id="gallery" name="img_placement[]" value="Gallery" <?php if (strpos($image_placement, "Gallery") !== FALSE) : ?>Checked <?php endif; ?> />
                                             Photo Gallery
                                         </label>
 
@@ -225,6 +225,7 @@ $image->store_result();
                                     </div>
                                     <div class="button-section my-3">
                                         <button class="btn-primary form-controls-btn" type="submit"><i class="fa-solid fa-floppy-disk"></i> Save Changes </button>
+                                        <a href="gallery" class="btn-primary btn-secondary form-controls-btn"><i class="fa-solid fa-ban"></i> Cancel Changes</a>
                                     </div>
                                     <div id="response" class="d-none">
                                         <p>Article Saved <img src="./assets/img/icons/check.svg" alt=""></p>
@@ -238,6 +239,50 @@ $image->store_result();
                             </div>
                         <?php endif; ?>
                     <?php endif; ?>
+                    <?php if ($_GET['action'] == "create") : ?>
+
+                        <div class="std-card">
+                            <h1>Upload New Image</h1>
+                            <form action="scripts/gallery.script.php" id="img-upload" method="POST" enctype="multipart/form-data">
+                                <div class="form-input-wrapper">
+                                    <label for="image_title">Image Title</label>
+                                    <!-- input -->
+                                    <input class="text-input input" type="text" name="image_title" id="image_title" placeholder="Image Title" maxlength="45">
+                                </div>
+                                <div class="form-input-wrapper">
+                                    <label for="image_description">Image Description</label>
+                                    <p class="form-hint-small">This is shown in your slideshow</p>
+                                    <!-- input -->
+                                    <input class="text-input input" type="text" name="image_description" id="image_description" placeholder="Image Description" maxlength="45">
+                                </div>
+                                <div class="form-input-wrapper my-2">
+                                    <label for="gallery_img">Upload Image</label>
+                                    <p class="form-hint-small">This can be in a JPG, JPEG or PNG format. And no larger than 1MB.</p>
+                                    <!-- input -->
+                                    <input type="file" name="gallery_img" id="gallery_img" accept="image/*">
+                                </div>
+
+                                <h3>Image Placement</h3>
+                                <div class="my-2">
+                                    <label class="checkbox-form-control" for="home">
+                                        <input type="checkbox" id="home" name="img_placement[]" value="Home" />
+                                        Home Screen
+                                    </label>
+                                    <label class="checkbox-form-control" for="gallery">
+                                        <input type="checkbox" id="gallery" name="img_placement[]" value="Gallery" />
+                                        Photo Gallery
+                                    </label>
+                                </div>
+
+                                <div class="button-section my-3">
+                                    <button class="btn-primary form-controls-btn loading-btn" type="submit"><span id="loading-btn-text" class="loading-btn-text"><i class="fa-solid fa-upload"></i>Upload Image</span> <img id="loading-icon" class="loading-icon d-none" src="./assets/img/icons/loading.svg" alt=""></button>
+                                    <a class="btn-primary btn-secondary" href="gallery" type="button"><i class="fa-solid fa-ban"></i>Cancel</a>
+                                </div>
+                            </form>
+                        </div>
+
+                    <?php endif; ?>
+
 
                     <?php if ($_GET['action'] == "view") : ?>
                         <?php if (($image->num_rows) > 0) :
@@ -268,13 +313,13 @@ $image->store_result();
 
 
 
-            </div>
+            
         <?php endif; ?>
 
     <?php else : ?>
         <p class="font-emphasis">You do not have the necessary Administrator rights to view this page.</p>
     <?php endif; ?>
-    </div>
+    
 
         </section>
 
@@ -288,14 +333,7 @@ $image->store_result();
     <!-- Footer -->
     <?php include("./inc/footer.inc.php"); ?>
     <!-- /Footer -->
-    <script>
-        $(".nav-btn").click(function() {
-            $(".nav-bar").fadeToggle(500);
-        });
-        $(".btn-close").click(function() {
-            $(".nav-bar").fadeOut(500);
-        })
-    </script>
+
     <script>
         //script for editing a news article
         $("#edit_image").submit(function(event) {
@@ -316,6 +354,42 @@ $image->store_result();
                     $("#response").html(data);
                     $("#response").slideDown(400);
                     window.location.replace('gallery');
+                }
+            });
+
+        });
+    </script>
+    <script>
+        //script for uploading a new image and posting to backend article
+        $("#img-upload").submit(function(event) {
+            event.preventDefault();
+
+
+            var formData = new FormData($("#img-upload").get(0));
+
+            formData.append("action", "newimg");
+
+            $.ajax({ //start ajax post
+                type: "POST",
+                url: "scripts/gallery.script.php",
+                data: formData,
+                contentType: false,
+                processData: false,
+                beforeSend: function() { //animate button
+                    $("#loading-icon").show(400);
+                },
+                complete: function() {
+                    $("#loading-icon").hide(400);
+                },
+                success: function(data, responseText) {
+                    $("#response").html(data);
+                    $("#response").slideDown(400);
+                    if (data === "success") {
+                        window.location.replace("gallery");
+                    }
+                    $("#img-upload")[0].reset();
+
+
                 }
             });
 
