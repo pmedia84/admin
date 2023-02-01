53561603cd56aa2d5e717f72423941e773aeea1c
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2023-02-01 14:03:20 +0000

    updates

--- inc/settings.php
+++ inc/settings.php
@@ -19,16 +19,7 @@ $cms_type = $cms_type;
 //email settings for contact forms
 //Settings for all form scripts
 
-/// Define who the emails get sent to from forms filled out
-$email_to = "";
-
-$host = "admin.parrotmedia.co.uk"; /// Hostname
-$username = "admin@admin.parrotmedia.co.uk"; ///Username
-$pass = "Krb833908"; /// Password
-$from = $username; ///Email address
-
-$fromname = "Parrot Media"; /// Username and how you want your name to be displayed on emails
-$emailheaderlogo = "https://www.parrotmedia.co.uk/img/pmedia-logo-new.png"; //logo url for inserting into the top of email bodies
+include("../email_settings.php");
 
 ////////////////Modules Available\\\\\\\\\\\\\\\\\\\\
 //connect to modules table and load available modules
