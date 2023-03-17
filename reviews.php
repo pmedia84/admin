bc3b5b20678013a3066f141eaf5d7c75aa998d08
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2023-03-17 14:07:32 +0000

    reviews error

--- reviews.php
+++ reviews.php
@@ -7,9 +7,9 @@ if (!$_SESSION['loggedin'] == TRUE) {
     header("Location: login.php?location=".$location);
 }
 
+include("./connect.php");
 include("inc/head.inc.php");
 include("inc/settings.php");
-include("./connect.php");
 //find if this module is on or off
 
 ////////////////Find details of the cms being used, on every page\\\\\\\\\\\\\\\
@@ -18,51 +18,8 @@ include("./connect.php");
 //business name
 $cms_name ="";
 $user_id = $_SESSION['user_id'];
-if ($cms_type == "Business") {
-    //look for the business set up and load information
-    //find business details.
-    $business_query = ('SELECT business_id, business_name FROM business');
-    $business = $db->query($business_query);
-    $business_details = mysqli_fetch_assoc($business);
-    $business = $db->prepare('SELECT * FROM business');
 
-    $business->execute();
-    $business->store_result();
-    $business->bind_result($business_id, $business_name, $address_id, $business_phone, $business_email, $business_contact_name);
-    $business->fetch();
-    $business->close();
-    //set cms name
-    $cms_name = $business_name;
-    //find user details for this business
-    $business_users = $db->prepare('SELECT users.user_id, users.user_name, business_users.business_id, business_users.user_type FROM users NATURAL LEFT JOIN business_users WHERE users.user_id='.$user_id);
 
-    $business_users->execute();
-    $business_users->bind_result($user_id, $user_name,$business_id, $user_type);
-    $business_users->fetch();
-    $business_users->close();
-}
-
-//run checks to make sure a wedding has been set up correctly
-if ($cms_type == "Wedding") {
-    //look for the Wedding set up and load information
-    //find Wedding details.
-    $wedding = $db->prepare('SELECT * FROM wedding');
-
-    $wedding->execute();
-    $wedding->store_result();
-    $wedding->bind_result($wedding_id, $wedding_name, $wedding_email, $wedding_phone, $wedding_contact_name);
-    $wedding->fetch();
-    $wedding->close();
-    //set cms name
-    $cms_name = $wedding_name;
-    //find user details for this business
-    $business_users = $db->prepare('SELECT users.user_id, users.user_name, business_users.business_id, business_users.user_type FROM users NATURAL LEFT JOIN business_users WHERE users.user_id='.$user_id);
-
-    $business_users->execute();
-    $business_users->bind_result($user_id, $user_name,$business_id, $user_type);
-    $business_users->fetch();
-    $business_users->close();
-}
 
 //////////////////////////////////////////////////////////////////Everything above this applies to each page\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 //find if this module is on or off
@@ -155,7 +112,7 @@ if ($cms_type == "Wedding") {
                 <p>Your 5 most recent reviews are displayed here and on your website.</p>
                 <p>This is updated once a week, you can also update it here by clicking the below button.</p>
                 <button class="btn-primary" id="get_reviews_btn" type="button"><i class="fa-solid fa-download"></i>Fetch Recent Reviews</button>
-                <?php if($user_type == "Admin"):?>
+                <?php if($user_type == "Admin" || $user_type=="Developer"):?>
                     <div id="reviews">
 
                     </div>
