2a403ff2b444146cd92125cee1cd7e4609132a67
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2023-05-16 14:34:52 +0100

    reviews

--- reviews.php
+++ reviews.php
@@ -9,7 +9,7 @@ if ($cms->type() == "Business") {
     //look for the business set up and load information
     //find business details.
     $business = $db->prepare('SELECT * FROM business');
-    
+
     $business->execute();
     $business->store_result();
     $business->bind_result($business_id, $business_name, $address_id, $business_phone, $business_email, $business_contact_name);
@@ -18,10 +18,10 @@ if ($cms->type() == "Business") {
     //set cms name
     $cms_name = $business_name;
     //find user details for this business
-    $business_users = $db->prepare('SELECT users.user_id, users.user_name, business_users.business_id, business_users.user_type FROM users NATURAL LEFT JOIN business_users WHERE users.user_id='.$user->user_id());
-    
+    $business_users = $db->prepare('SELECT users.user_id, users.user_name, business_users.business_id, business_users.user_type FROM users NATURAL LEFT JOIN business_users WHERE users.user_id=' . $user->user_id());
+
     $business_users->execute();
-    $business_users->bind_result($user_id, $user_name,$business_id, $user_type);
+    $business_users->bind_result($user_id, $user_name, $business_id, $user_type);
     $business_users->fetch();
     $business_users->close();
 }
@@ -50,7 +50,7 @@ include("inc/head.inc.php");
 
 
         <!-- Header Section -->
-            <?php include("inc/header.inc.php");?>
+        <?php include("inc/header.inc.php"); ?>
         <!-- Nav Bar -->
         <?php include("./inc/nav.inc.php"); ?>
         <!-- /nav bar -->
@@ -61,26 +61,26 @@ include("inc/head.inc.php");
                 <a href="index.php" class="breadcrumb">Home</a> / Manage Reviews
             </div>
             <div class="main-cards">
-                <?php if($reviews->status() =="On"):?>
-                    
-                <h1>Reviews</h1>
-                <p>Your 5 most recent reviews are displayed here and on your website.</p>
-                <p>This is updated once a week, you can also update it here by clicking the below button.</p>
-                <button class="btn-primary" id="get_reviews_btn" type="button"><i class="fa-solid fa-download"></i>Fetch Recent Reviews</button>
-                <?php if($user_type == "Admin" || $user_type=="Developer"):?>
-                    <div id="reviews">
+                <?php if ($reviews->status() == "On") : ?>
 
-                    </div>
+                    <h1>Reviews</h1>
+                    <p>Your 5 most recent reviews are displayed here and on your website.</p>
+                    <p>This is updated once a week, you can also update it here by clicking the below button.</p>
+                    <button class="btn-primary" id="get_reviews_btn" type="button"><i class="fa-solid fa-download"></i>Fetch Recent Reviews</button>
+                    <?php if ($user_type == "Admin" || $user_type == "Developer") : ?>
+                        <div id="reviews">
+
+                        </div>
             </div>
 
-                <?php else:?>
-                    <p class="font-emphasis">You do not have the necessary Administrator rights to view this page.</p>
-                <?php endif; ?>
-            </div>
-            <?php else:?>
-                <h1>Module not activated for your website!</h1>
-                <p>Contact us to find out how you can get this feature set up.</p>
-            <?php endif;?>        
+        <?php else : ?>
+            <p class="font-emphasis">You do not have the necessary Administrator rights to view this page.</p>
+        <?php endif; ?>
+        </div>
+    <?php else : ?>
+        <h1>Module not activated for your website!</h1>
+        <p>Contact us to find out how you can get this feature set up.</p>
+    <?php endif; ?>
         </section>
 
 
